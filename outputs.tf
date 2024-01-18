690ec250c84da2e30143be861bac6f09431147b9
CodingManoj <CodingManoj@gmail.com>
2024-01-17 19:59:16 -0500

    TF Backend Infra Auto Push

--- outputs.tf
+++ outputs.tf
@@ -2,44 +2,10 @@
 #     value  = module.vpc.VPC_ID
 # }
 
-# output "VPC_CIDR" {
-#     value  = module.vpc.VPC_CIDR
-# }
+output "PUBLIC_ALB_ARN" {
+    value = module.alb_public.ALB_ARN
+}
 
-# output "PRIVATE_SUBNET_IDS" {
-#     value  = module.vpc.PRIVATE_SUBNET_IDS
-# }
-
-# output "PUBLIC_SUBNET_IDS" {
-#     value  = module.vpc.PUBLIC_SUBNET_IDS
-# }
-
-# output "PRIVATE_SUBNET_CIDR" {
-#     value  = module.vpc.PRIVATE_SUBNET_CIDR
-# }
-
-# output "PUBLIC_SUBNET_CIDR" {
-#     value  = module.vpc.PRIVATE_SUBNET_CIDR
-# }
-
-# output "DEFAULT_VPC_CIDR" {
-#     value   = module.vpc.DEFAULT_VPC_CIDR
-# }
-
-# output "DEFAULT_VPC_ID" {
-#     value   = module.vpc.DEFAULT_VPC_ID
-# }
-
-# output "PUBLIC_HOSTED_ZONE_ID" {
-#     value = var.PUBLIC_HOSTED_ZONE_ID
-# }
-# output "PUBLIC_HOSTED_ZONE_NAME" {
-#     value = var.PUBLIC_HOSTED_ZONE_NAME
-# }
-
-# output "PRIVATE_HOSTED_ZONE_ID" {
-#     value = var.PRIVATE_HOSTED_ZONE_ID
-# }
-# output "PRIVATE_HOSTED_ZONE_NAME" {
-#     value = var.PRIVATE_HOSTED_ZONE_NAME
-# }
+output "PRIVATE_ALB_ARN" {
+    value = module.alb_private.ALB_ARN
+}
