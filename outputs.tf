c4bdac00751440975a0dfa87fbc82c9e2d8254ac
CodingManoj <CodingManoj@gmail.com>
2024-01-17 20:00:03 -0500

    TF Backend Infra Auto Push

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@
 # }
 
 output "PUBLIC_ALB_ARN" {
-    value = module.alb_public.ALB_ARN
+    value = module.alb-public.ALB_ARN
 }
 
 output "PRIVATE_ALB_ARN" {
-    value = module.alb_private.ALB_ARN
+    value = module.alb-private.ALB_ARN
 }
